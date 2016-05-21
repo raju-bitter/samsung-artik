216606b701794b1ea8cfcf0f3847571ade2e75cf
Raju Bitter <rajubitter@gmail.com>
2016-05-21 21:52:12 +0200

    Build scripts for fusing and setup added

---
+++ artik10-tizen/setup.sh
@@ -0,0 +1,13 @@
+# Check out Kernel source with correct brand, and configure customization for Artik
+
+TIZEN_USER=rteem
+
+# git clone ssh://TIZEN_USER@review.tizen.org:29418/platform/kernel/linux-exynos
+# cd linux-exynos
+# git checkout -b tizen origin/tizen
+
+cp ../exynos5422-artik10.dts arch/arm/boot/dts/exynos5422-artik10.dts
+
+cp ../artik10_defconfig arch/arm/configs/artik10_defconfig
+cp ../build_kernel.sh .
+./build_kernel.sh
